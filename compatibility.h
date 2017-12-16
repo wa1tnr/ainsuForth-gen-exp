1e71347ad8a2975bb3d5be69f624cf47a975e514
wa1tnr <wa1tnr@users.noreply.github.com>
2017-12-16 01:50:09 +0000

    new file:   0-Distribution.d/YAFFA-ARM-master/c0ffee 	new file:   0-dist.d/c0ffee 	new file:   AUTHORS 	new file:   BROKEN_NOT_ALL_UPLOADED 	new file:   ChangeLog 	new file:   Dictionary.h 	new file:   Error_Codes.h 	new file:   INSTALL 	new file:   SUCCESS 	new file:   WTF.md 	new file:   ainsuForth-gen-exp.ino 	new file:   ainsuForthsketch.cpp 	new file:   ainsuForthsketch.h 	new file:   compatibility.h 	new file:   doc/examples/c0ffee 	new file:   flashDict.cpp 	new file:   flashDict.h 	new file:   src/dict/c0ffee 	new file:   src/kernel/c0ffee 	new file:   src/periph/c0ffee 	new file:   yaffa.h initial population of the project - manual clone.

---
+++ compatibility.h
@@ -0,0 +1,43 @@
+// Sat Dec 16 01:24:37 UTC 2017
+// 4737-a0a-00a-
+
+
+// previous timestamps:
+// Sun Dec 10 22:48:03 UTC 2017
+// 4735-b0d-00b-   the -00x- is new Dec 10, 2017.
+
+// Wed Aug  2 18:04:12 UTC 2017
+// 4735-b0c-05-
+
+// packages/adafruit/hardware/samd/1.0.18/boards.txt:173:adafruit_gemma_m0.build.extra_flags=-DCRYSTALLESS -DADAFRUIT_GEMMA_M0 -D__SAMD21E18A__ {build.usb_flags}
+
+// In July 2017, all boards were SAMD21G18A except Gemma M0, which is SAMD21E18A (similar inside, fewer pins brought out?)
+
+// GEMMA M0
+// + dotstar x1
+// CRYSTALLESS   ADAFRUIT_GEMMA_M0   __SAMD21E18A__
+
+// FEATHER M0
+// ARDUINO_SAMD_ZERO __SAMD21G18A__
+
+// FEATHER M0 EXPRESS
+// + npx x1  + SPI flash 2MB
+// ARDUINO_SAMD_ZERO ARDUINO_SAMD_FEATHER_M0 ADAFRUIT_FEATHER_M0_EXPRESS __SAMD21G18A__ 
+
+// RADIO M0 (Feather ..)
+// __SAMR21G18A__ 
+
+// METRO M0 (Metro M0 Express)
+// + npx x1  + SPI flash 2MB
+// __SAMD21G18A__  ARDUINO_SAMD_ZERO ADAFRUIT_METRO_M0_EXPRESS 
+
+
+// CPX - Circuit Playground M0 (Circuit Playground Express)
+// + npx x10  + SPI flash 2MB
+// CRYSTALLESS ARDUINO_SAMD_ZERO __SAMD21G18A__ ADAFRUIT_CIRCUITPLAYGROUND_M0
+
+// reverse these two lines to enable SPI flashROM support:
+#define HAS_SPI_FLASH_DEMO
+#undef HAS_SPI_FLASH_DEMO
+// load echo! remove download are relevant only to presence of SPI flashROM.
+
